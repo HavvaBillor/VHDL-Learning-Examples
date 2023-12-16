c083264dbd4aecbe743e48d1c90501d6afe97a9e
Havva Billor <havvabillorx@gmail.com>
2023-12-16 20:29:51 +0300

    first vhdl code added

---
+++ The gates/1. and gate.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity and2 is
+    Port ( in1 : in STD_LOGIC;
+           in2 : in STD_LOGIC;
+           out1 : out STD_LOGIC);
+end and2;
+
+architecture arc_and2 of and2 is
+
+begin
+    out1 <= in1 and in2;
+
+end arc_and2;
